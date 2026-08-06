94499671972e0668c5a9de150fc0f182f9e8d0aa
Ayush Umrao <umraoayush@gmail.com>
2026-08-06 23:58:49 +0530

    feat(todo): add initial sample todo entries

---
+++ app/data/todos.py
@@ -0,0 +1,16 @@
+from typing import List,Dict,Any
+
+todos_db : List[Dict[str,Any]] = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI Structure",
+        "description": "Understand modular folder layout",
+        "is_completed": False,
+    },
+    {
+        "id": 2,
+        "title": "Connect with Flutter",
+        "description": "Integrate API using Dio",
+        "is_completed": False,
+    },
+]
