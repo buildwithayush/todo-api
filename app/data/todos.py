7fe8fea2a700980676c7c6c6eaa3e2abe15018af
Ayush Umrao <umraoayush@gmail.com>
2026-08-11 23:17:52 +0530

    feat(todo): create TodoDB model for database integration

--- app/data/todos.py
+++
@@ -1,6 +0,0 @@
-
-# Fake database (In-Memory List)
-todos_db = [
-    {"id": 1, "title": "Buy Groceries", "description": "Milk, Eggs, Bread", "completed": False},
-    {"id": 2, "title": "Learn FastAPI", "description": "Understand Pydantic and Routes", "completed": True}
-]
