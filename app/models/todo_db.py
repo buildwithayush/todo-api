105acd5130be28d21d4bb589488b1bc81d0a38c1
Ayush Umrao <umraoayush@gmail.com>
2026-08-12 21:32:00 +0530

    feat(todo): update TodoDB model to use Mapped for type annotations

--- app/models/todo_db.py
+++ app/models/todo_db.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column,Integer, String, Boolean
 from app.database import Base
+from sqlalchemy.orm import Mapped,mapped_column
 
 class TodoDB(Base):
     __tablename__ = "todos" 
 
-    id = Column(Integer, primary_key=True, index=True) 
-    title = Column(String, nullable=False)             
-    description = Column(String, nullable=True)        
-    completed = Column(Boolean, default=False)
+    id : Mapped[int] =  mapped_column(Integer, primary_key=True, index=True) 
+    title : Mapped[str] = mapped_column(String, nullable=False)             
+    description: Mapped[str | None] = mapped_column(String, nullable=True)        
+    completed : Mapped[bool] = mapped_column(Boolean, default=False)
