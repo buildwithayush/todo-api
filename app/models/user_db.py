aa194b19e7b422f0c7bc5145a29b2890d2ca2165
Ayush Umrao <umraoayush@gmail.com>
2026-08-13 21:56:30 +0530

    feat(user): create UserDB model for user data management

---
+++ app/models/user_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer,Boolean,String
+from app.database import Base
+from sqlalchemy.orm import Mapped,mapped_column
+
+class UserDB(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email:Mapped[str] = mapped_column(String,unique=True, index=True, nullable=False)
+    hashed_password:Mapped[str] = mapped_column(String,nullable=False)
+    is_active:Mapped[bool]= mapped_column(Boolean,default=True)
+
