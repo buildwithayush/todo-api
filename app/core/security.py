49b0e6bd6e164169e58f04897f99caad08ecc3fe
Ayush Umrao <umraoayush@gmail.com>
2026-08-16 11:53:35 +0530

    feat(security): add JWT token creation function and update security settings

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,11 @@
 from pwdlib import PasswordHash
+import jwt
+from datetime import datetime,timedelta,timezone
 
+
+SECRET_KEY = 'SUPER_SECRET_KEY'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = PasswordHash.recommended()
 
 def hash_password(plain_password:str) -> str:
@@ -7,3 +13,12 @@ def hash_password(plain_password:str) -> str:
 
 def verify_password(plain_password:str , hashed_password:str) -> bool:
     return pwd_context.verify(plain_password,hashed_password)
+
+def create_access_token(data:dict) -> str:
+    to_encode= data.copy()
+    
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp':expire})
+
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,ALGORITHM)
+    return encoded_jwt
