44980b38bd0b30fee396696eaf0d5c86daa17bf1
Ayush Umrao <umraoayush@gmail.com>
2026-08-16 21:08:11 +0530

    feat(cors): add CORS middleware to enable cross-origin requests

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 from app.database import engine,Base
 from app.routes.todo_routes import router as todo_router
 from app.routes.todo_routes import auth_router as todo_auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title='Todo APi',
     version='1.0.0'
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],         
+    allow_methods=["*"],            
+    allow_headers=["*"],         
 )
 
 app.include_router(todo_router, prefix='/api/v1')
