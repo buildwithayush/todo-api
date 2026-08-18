3bc5d69c712e7297d57f4bd9199eedd0dac63231
Ayush Umrao <umraoayush@gmail.com>
2026-08-18 18:20:43 +0530

    feat(tests): add authentication tests for signup and login functionality

---
+++ tests/test_auth.py
@@ -0,0 +1,34 @@
+def test_signup_success(client):
+    #  API Call
+    payload = {
+        "email": "tester@example.com",
+        "password": "strongpassword123"
+    }
+    response = client.post("/api/v1/auth/signup", json=payload)
+    
+    #  Assertions 
+    assert response.status_code == 201
+    data = response.json()
+    assert data["email"] == "tester@example.com"
+    assert "id" in data
+    assert "password" not in data  
+
+def test_signup_duplicate_email(client):
+    
+    payload = {
+        "email": "tester@example.com",
+        "password": "strongpassword123"
+    }
+    response = client.post("/api/v1/auth/signup", json=payload)
+    assert response.status_code == 400
+    assert response.json()["detail"] == 'Email Already Registered'
+
+def test_login_wrong_password(client):
+    payload ={
+        'email' :'tester@example.com',
+        'password':'strongpassword12'
+    }
+    response = client.post('/api/v1/auth/login',json=payload)
+    assert response.status_code == 401
+    assert response.json()['detail'] == 'Invalid Credentials'
+        
