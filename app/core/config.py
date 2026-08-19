647628c4c8ba154669b34830befb08dce71ad9dd
Ayush Umrao <umraoayush@gmail.com>
2026-08-19 20:50:37 +0530

    feat(config): add TEST_DATABASE_URL to Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = ""
+    TEST_DATABASE_URL: str =""
     SECRET_KEY: str = "default_secret_key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
